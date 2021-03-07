b1684b51cd9ba3934c8be11ba07d9cfa18b6a4cc
Gurthyy <kjschultz08@gmail.com>
2021-03-07 00:43:30 -0700

    Build nav component and register

---
+++ src/app/nav/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nav-bar',
+  templateUrl: 'navbar.component.html',
+})
+export class NavBarComponent {
+  constructor() {}
+
+  ngOnInit() {}
+}
