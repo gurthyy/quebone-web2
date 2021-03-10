48bb0da833c774cbe9de28a3af3136522e79117a
Gurthyy <kjschultz08@gmail.com>
2021-03-09 23:47:13 -0700

    Rework modules and dependancies. Add reset function.

---
+++ src/app/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,35 @@
+import { Component, Inject } from '@angular/core';
+import { SharedModule } from '../shared/shared.module'
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'qb-confirm-dialog',
+  templateUrl: './confirm-dialog.component.html',
+  styles: ['']
+})
+export class ConfirmDialogComponent {
+
+  title: string;
+  message: string;
+
+  constructor(public dialogRef: MatDialogRef<ConfirmDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: ConfirmDialogModel) {
+    this.title = data.title;
+    this.message = data.message
+  }
+
+  onConfirm(): void {
+    // Close dialog, return true
+    this.dialogRef.close(true);
+  }
+
+  onDismiss(): void{
+    // Close dialog, return false
+    this.dialogRef.close(false)
+  }
+}
+
+export class ConfirmDialogModel{
+  constructor(public title: string, public message: string){}
+}
+
+
